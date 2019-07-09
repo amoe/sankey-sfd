02d4f09fcd3fe2e7612d11b3be47ec6d30943c0c
David Banks <amoebae@gmail.com>
2019-07-09 14:15:04 +0100

    working version

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function onReady() {
 
     console.log("selection is %o", svg);
 
+    // Draw the nodes as rectangles
     svg.append("g")
         .attr("stroke", "#000")
         .selectAll("rect")
@@ -30,10 +31,38 @@ function onReady() {
         .attr("y", d => d.y0)
         .attr("height", d => d.y1 - d.y0)
         .attr("width", d => d.x1 - d.x0)
-        .attr("fill", d => "black")
+        .attr("fill", d => "green")
         .append("title")
         .text(d => `${d.name}\n${d.value}`);
 
+    // Draw labels for the nodes
+    svg.append("g")
+        .style("font", "10px sans-serif")
+        .selectAll("text")
+        .data(graph.nodes)
+        .join("text")
+        .attr("x", d => d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6)
+        .attr("y", d => (d.y1 + d.y0) / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
+        .text(d => d.name);
+
+
+    const link = svg.append("g")
+          .attr("fill", "none")
+          .attr("stroke-opacity", 0.5)
+          .selectAll("g")
+          .data(graph.links)
+          .join("g")
+          .style("mix-blend-mode", "multiply");
+
+    link.append("path")
+        .attr("d", sankeyLinkHorizontal())
+        .attr("stroke",  "#aaaaaa")
+        .attr("stroke-width", d => Math.max(1, d.width));
+
+  link.append("title")
+      .text(d => `${d.source.name} → ${d.target.name}\n${d.value}`);
 }
 
 document.addEventListener('DOMContentLoaded', onReady);
